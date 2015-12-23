a26ced6a4cfe1689a3cf748e599a45401cae4b52
gator <791264638@qq.com>
2015-12-23 17:31:08 +0800

    微信消息推送类(PHP)

---
+++ sendMessage.php
@@ -0,0 +1,82 @@
+<?php
+//微信发送模板消息给用户
+class sendMessage{
+	public function StartSendMessage($appid, $secrect,$touser,$template_id,$url,$data,$topcolor='#7B68EE'){
+		//此处应该对access_token做保存(有待完善,由于触发少,应该不会出现问题)
+		$this->accessToken = $this->getToken($appid, $secrect);
+		$re=$this->Send($touser,$template_id,$url,$data,$topcolor='#7B68EE');
+		if($re){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//发送消息,组装数据
+	public function Send($touser,$template_id,$url,$data,$topcolor='#7B68EE'){
+		$template = array(
+			'touser' => $touser,
+			'template_id' => $template_id,
+			'url' => $url,
+			'topcolor' => $topcolor,
+			'data' => $data
+		);
+		$json_template = json_encode($template);
+		$url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" . $this->accessToken;
+		$dataRes = $this->request_post($url, urldecode($json_template));
+		if($dataRes['errcode']==0){
+			return true;
+		}else{
+			return false;
+		}		
+	}
+	//post发送请求
+	function request_post($url,$param){
+		$oCurl = curl_init();
+		if(stripos($url,"https://")!==FALSE){
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+		}
+		if (is_string($param)) {
+			$strPOST = $param;
+		} else {
+			$aPOST = array();
+			foreach($param as $key=>$val){
+				$aPOST[] = $key."=".urlencode($val);
+			}
+			$strPOST =  join("&", $aPOST);
+		}
+		curl_setopt($oCurl, CURLOPT_URL, $url);
+		curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt($oCurl, CURLOPT_POST,true);
+		curl_setopt($oCurl, CURLOPT_POSTFIELDS,$strPOST);
+		$sContent = curl_exec($oCurl);
+		$aStatus = curl_getinfo($oCurl);
+		curl_close($oCurl);
+		return $sContent;
+	}
+	//get发送请求
+	function request_get($url){
+		 $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        if (!empty($data)){  
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        return $output;
+	}
+	//获取access_token
+	public function getToken($appid,$appsecret){
+		$url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" . $appid . "&secret=" . $appsecret;
+		$token = $this->request_get($url);
+		//去掉特殊字符,在此不做处理
+		//$token = json_decode(stripslashes($token));
+		$arr = json_decode($token,true);
+		$access_token = $arr['access_token'];
+		return $access_token;
+	}
+}
